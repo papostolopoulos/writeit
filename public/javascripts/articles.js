1257fe54a750ab3986ae7f8a22c7f56d3847b710
Paraskevas Apostolopoulos <papostolopoulos@gmail.com>
2017-02-06 16:18:37 -0800

    safety push

---
+++ public/javascripts/articles.js
@@ -0,0 +1,8 @@
+//RENDER ALL THE ARTICLES BY LOOPING
+
+let iFrameArticles = document.getElementsByClassName("iFrameArticles");
+let hiddenSpan = document.getElementsByClassName("hiddenSpan");
+
+for (let i = 0; i < iFrameArticles.length; i++) {
+  iFrameArticles[i].contentDocument.body.innerHTML = hiddenSpan[i].getAttribute("value");
+}
