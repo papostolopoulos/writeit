0089bfbec4f3b516779d3e97fddd514d7c6d034e
Paraskevas Apostolopoulos <papostolopoulos@gmail.com>
2017-02-14 10:12:01 -0800

    commiting before switching branches

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,4 +1,20 @@
-console.log("In the scripts file");
-let imagesTags = document.getElementsByTagName('img');
-let iFrameArticles = document.getElementsByClassName("iFrameArticles")
-console.log(iFrameArticles[0].contentDocument.body.childNodes);
+// console.log("In the scripts file");
+// let iFrameArticles = document.getElementsByClassName("iFrameArticles")
+// // console.log(typeof document.getElementsByTagName("body")[0].offsetWidth);
+// if (document.getElementsByTagName("body")[0].offsetWidth <= 768) {
+//   console.log("screen is mobile sized");
+//   for (let i = 0; i < iFrameArticles.length; i++) {
+//     iFrameArticles[i].contentDocument.body.getElementsByTagName("img").clientWidth = 320;
+//     iFrameArticles[i].contentDocument.body.getElementsByTagName("img").naturalWidth = 320;
+//     iFrameArticles[i].contentDocument.body.getElementsByTagName("img").offsetWidth = 320;
+//     iFrameArticles[i].contentDocument.body.getElementsByTagName("img").width = 320;
+//
+//
+//
+//     console.log(iFrameArticles[i].contentDocument.body.getElementsByTagName("img"));
+//     // for (let j = 0; j < iFrameImages.length; j++) {
+//     //   console.log(iFrameImages[j]);
+//     // }
+//   }
+// }
+// console.log(iFrameArticles[0].contentDocument.body.getElementsByTagName("img"));
